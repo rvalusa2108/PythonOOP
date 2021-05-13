4329c84c0016cb3d300fd6ca759cf701512efe87
Raghu Valusa <raghu.valusa@gmail.com>
2021-05-14 00:33:10 +1000

    Added the inheritence class

---
+++ PythonInheritence.py
@@ -0,0 +1,7 @@
+# from PythonOOP import PythonInheritence as PyInh
+from PythonInheritence import pythoninheritence as PyInh
+
+if __name__ == '__main__':
+    pyInheritenceObj = PyInh.myBird()
+
+
---
+++ PythonInheritence/pythoninheritence.py
@@ -0,0 +1,10 @@
+# Creating Class and Object in Python
+class myBird:
+    def __init__(self):
+        print("myBird class constructor is executing...")
+
+    def whatType(self):
+        print("I am a Bird...")
+
+    def canSwim(self):
+        print("I can Swim...")
